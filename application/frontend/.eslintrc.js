0f031b2ddec44f88e8f50c6770abd581a1b285d4
Sam <mac@MACs-MacBook-Pro.local>
2025-10-21 16:24:57 +0100

    now update

--- application/frontend/.eslintrc.js
+++ application/frontend/.eslintrc.js
@@ -1,37 +1,24 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
+    'next/core-web-vitals',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
   ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
   root: true,
   env: {
     browser: true,
     es2021: true,
     node: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', 'node_modules/', '.next/', 'out/'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': 'warn',
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
     'prettier/prettier': 'warn',
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
   },
 };
